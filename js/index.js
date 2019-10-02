c09e388cb56162d21e3ef2c65b4a9128c957e937
Soraia <soraia@Soraias-iMac.local>
2019-10-01 23:39:13 -0400

    I've created 5 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
-// Your code goes here
+/**     MouseEnter and MouseLeave Event   **/
+
+const canalImage = document.querySelector(".img-fluid");
+// console.log(canalImage);
+
+canalImage.addEventListener("mouseenter", () => {
+canalImage.style.transform = "scale(2.1)";
+canalImage.style.transition = "all 0.8s";
+})
+canalImage.addEventListener("mouseleave", () => {
+    canalImage.style.transform = "scale(1.0)";
+    canalImage.style.transition = "all 0.8s";
+})
+
+/**          Click Event            **/
+const nav = document.querySelectorAll("a");
+// console.log(nav);
+
+nav.forEach(element => {
+    element.addEventListener("click", () => {
+        element.style.color = "dodgerBlue";
+    });
+})
+
+/**           Click Event           **/
+
+const button = document.querySelector(".intro h2 ");
+    console.log(button);
+
+    button.addEventListener("click", (event) => {
+        event.target.textContent = "Enjoy your ride!";
+        event.target.style.color = "hotpink";
+        event.target.style.transform = "scale(1.5)";
+    })
+
+
+/**           Scroll Event           **/
+window.addEventListener("scroll", () => {
+    ////this shows the amount of pixels of scroll without actually scrolling all the way down 
+    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+    ////this shows the amount of pixels as you are scrolling down
+    const scrolled = window.scrollY;
+
+    if(scrolled === scrollable) {
+        alert("You scrolled to the bottom!")
+    }
+});
+
+
+
+    
